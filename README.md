098d41309a349ab84f2d4ef062f0a58c36bcc0cf
liangtianfu <liangtianfu985@126.com>
2019-04-23 13:03:46 +0800

    初始化项目完成

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # shop-admin
+
+## 下载所需要的依赖包
+```
+npm install
+```
+
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- 调用Element UI组件 -->
+    <el-row>
+      <el-button type="primary">按钮</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  mounted() {
+    // 请求数据
+    this.$axios.get("https://api.github.com/users").then(res => {
+      console.log(res.data);
+    });
+  }
+};
+</script>
+
+<style>
+</style>
